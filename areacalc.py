d177a0a0e0bab93aeaee6b811d75234e8b113d3e
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-09 11:45:33 -0600

    Basic area calculator to practice my python fundamentals.

---
+++ areacalc.py
@@ -0,0 +1,7 @@
+print("Area Calculator version 1.23")
+
+length = float(input("\nPlease enter length: "))
+width = float(input("Please enter width: "))
+
+area = length * width
+print(f"The area of your given dimensions is: {area}")
