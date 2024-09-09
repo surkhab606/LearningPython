5f674b7d0a7945fcf699fce6b2a4c88b51a74b39
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-09 11:41:16 -0600

    Added comments, fleshed out story and added more variables for the user to mess around with

--- madlibs.py
+++ madlibs.py
@@ -2,7 +2,14 @@
 # User input
 adjective = input("Please enter an adjective: ")
 noun = input("Please enter a noun: ")
+adjective2 = input("Please enter a second adjective: ")
+noun2 = input("Please enter a second noun: ")
+verb = input("Please enter a verb: ")
+verb2 = input("Please enter a second verb: ")
 
-
-print(f"Today I went to school. It was very {adjective}")
+# Mad libs story
+print(f"Today I went to school. It was {adjective}.")
 print(f"Then I went to {noun}. It was great!")
+print(f"But the worst part of my day had to be when my mom forced me to come with her to {noun2}.")
+print(f"We had to {verb} there! It was {adjective2}!!")
+print(f"But whatever, at least we didn't have to {verb2} to get there!")
