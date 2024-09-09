969144445a94549cc1326a4de5415262104b75b8
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-09 11:36:44 -0600

    Mad libs python project to start from the basics and build up my learning!

---
+++ madlibs.py
@@ -0,0 +1,8 @@
+
+# User input
+adjective = input("Please enter an adjective: ")
+noun = input("Please enter a noun: ")
+
+
+print(f"Today I went to school. It was very {adjective}")
+print(f"Then I went to {noun}. It was great!")
