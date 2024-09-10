26dfe8af6cdb6c09b363208edec4082bf5ca042e
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-10 16:39:12 -0600

    Reviewed typecasting.

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,2 +1,13 @@
 name = "Surkhab"
-print(f"Hello {name}")
+price = 4.99
+age = 23
+condition = True
+
+age = float(age)
+price = int(price)
+condition = str(condition)
+name = bool(name)
+age = bool(age)
+
+
+
