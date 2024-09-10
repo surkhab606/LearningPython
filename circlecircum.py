fbd42a75f2ecadecb8e5c54a763e5c91f1373010
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-10 16:55:35 -0600

    Created circumference of a circle program.

--- circlecircum.py
+++ circlecircum.py
@@ -3,4 +3,4 @@ import math
 radius = float(input("Please enter the radius of the circle: "))
 circumference = 2 * radius * math.pi
 
-print(f"The circumference is {circumference}.")
+print(f"The circumference is equal to: {circumference}.")
