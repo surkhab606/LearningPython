0fa23b9e687eb50f7714f3b6bda52f36408be5d0
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-10 17:00:13 -0600

    Created hypotenuse of a triangle program using user inputs for sides a and b.

---
+++ hypocalc.py
@@ -0,0 +1,8 @@
+import math
+
+a = float(input("Please enter the first side of the triangle: "))
+b = float(input("Please enter the second side of the triangle: "))
+
+c = round(math.sqrt(pow(a, 2) + pow(b, 2)))
+
+print(f"The hypotenuse given your dimensions is equal to: {c}")
