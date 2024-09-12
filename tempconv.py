18f02bdb64532ad58ce7b29f45a2c0b0ffab9d9a
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-12 17:14:04 -0600

    Added rounding

--- tempconv.py
+++ tempconv.py
@@ -1,12 +1,12 @@
 userInput = int(input("Press 1 to convert C to F. Press 2 to convert F to C. "))
 
 if userInput == 1:
-    prompt1 = float(input("What is the temperature you would like to convert? "))
+    prompt1 = round(float(input("What is the temperature you would like to convert? ")))
     result1 = prompt1 * 1.8 + 32
     print(f"The temperature you inputted in Fahrenheit is: {result1} Fahrenheit")
 
 elif userInput == 2:
-    prompt2 = float(input("What is the temperature you would like to convert? "))
+    prompt2 = round(float(input("What is the temperature you would like to convert? ")))
     result2 = (prompt2 - 32) / 1.8
     print(f"The temperature you inputted in Celsius is: {result2} Celsius")
 
