143fe2ff3f32bc2b63193baa1afc017e86438f1b
surkhabmundi <surkhab.mundi@ucalgary.ca>
2024-09-19 13:37:22 -0600

    Learning for loops.

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,7 +1,5 @@
-
-age = int(input("Enter a number between 1 and 10: "))
-
-while age < 1 or age > 10:
-   age = int(input("Please do not enter an invalid number. Try again: "))
-
-print(f"Thank you. Your age is now {age} years old.")
+for x in range(1, 21):
+    if x == 13:
+        continue
+    else:
+        print(x)
