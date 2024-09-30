075f64851e5c0cf5aae610704a11573125a81355
surkhabmundi <surkhabm1@gmail.com>
2024-09-30 00:56:14 -0600

    Setup questions. Now setting up answers.

--- quizgame.py
+++ quizgame.py
@@ -1,6 +1,10 @@
-questions = ()
+questions = ("What is the smallest country in the world?",
+             "What is the only continent without an active volcano?",
+             "Which planet is known as the 'Morning Star' or the 'Evening Star'?",
+             "Which planet in our solar system has the most moons?",
+             "What is the name of the first satellite sent into space?")
 
-options = ((), (), (), (), ())
+options = (("", "", "", ""), (), (), (), ())
 
 answers = ()
 guesses = []
