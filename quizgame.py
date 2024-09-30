894bf1450d4da52e6b06f91e93d37caf22981c03
surkhabmundi <surkhabm1@gmail.com>
2024-09-30 01:05:56 -0600

    Done quiz game! Utilized for loops, nested loops, tuples, lists, print statements, user input and math

--- quizgame.py
+++ quizgame.py
@@ -10,7 +10,39 @@ options = (("A. Monaco", "B. Vatican City", "C. San Marino", "D. Liechtenstein")
            ("A. Jupiter", "B. Saturn", "C. Uranus", "D. Neptune"),
            ("A. Sputnik 1", "B. Explorer 1", "C. Apollo 1", "D. Apollo 10"))
 
-answers = ()
+answers = ("B", "C", "B", "B", "A")
 guesses = []
 score = 0
 question_num = 0
+
+for question in questions:
+    print("-----------------------------------")
+    print(question)
+    for option in options[question_num]:
+        print(option)
+    guess = input("Enter (A,B,C,D): ").upper()
+    guesses.append(guess)
+    if guess == answers[question_num]:
+        score += 1
+        print("CORRECT!")
+    else:
+        print("INCORRECT")
+        print(f"{answers[question_num]} is the correct answer")
+    question_num += 1
+
+print("-----------------------------------")
+print("             RESULTS               ")
+print("-----------------------------------")
+
+print("answers: ", end="")
+for answer in answers:
+    print(answer, end=" ")
+print()
+
+print("guesses: ", end="")
+for guess in guesses:
+    print(guess, end=" ")
+print()
+
+score = int(score / len(questions) * 100)
+print(f"Your score is {score}%")
