ea31fc9774745da7e07a0076c194031426e36e10
surkhabmundi <surkhabm1@gmail.com>
2024-10-02 00:20:20 -0600

    Practicing dictionaries.

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,5 +1,8 @@
-questions = ("What is the largest planet in our solar system?",
-             "What is the largest moon in our solar system?",
-             "What is the largest star in the Milky Way (discovered so far)?",
-             "What is a quasar?",
-             "Where are your fingers?")
+models = {"CLE53": "AMG",
+          "CLE63": "AMG",
+          "CLE300": "Base",
+          "CLE450": "AMG-line"}
+
+models.update({"CLE63S": "AMG Performance"})
+print(models)
+
