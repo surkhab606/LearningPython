238f08c916f9731188b496374515b52cdf94e8ca
surkhabmundi <surkhabm1@gmail.com>
2024-10-03 02:56:35 -0600

    Number guessing tutorial that is a little bit more advanced but not by much.

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -4,15 +4,19 @@ low = 1
 high = 100
 guess = random.randint(low, high)
 
-userGuess = int(input("Guess a number between 1 and 100: "))
+userGuess = int(input("Please guess a number between 1 and 100: "))
 
 while userGuess != guess:
-    if userGuess < guess:
-        print("Too low! Guess again!")
-        userGuess = int(input("Guess a number between 1 and 100: "))
-
-    elif userGuess > guess:
+    if userGuess > guess:
         print("Too high! Guess again!")
-        userGuess = int(input("Guess a number between 1 and 100: "))
+        userGuess = int(input("Please guess a number between 1 and 100"))
+
+    elif userGuess < guess:
+        print("Too low! Guess again!")
+        userGuess = int(input("Please guess a number between 1 and 100"))
+
+    else:
+        print("Invalid input. Guess again")
+        userGuess = int(input("Please guess a number between 1 and 100"))
 
 print("You got it!")
