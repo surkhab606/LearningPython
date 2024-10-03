ce14f6a9519873e0c84fe4a3584d2b0416780d23
surkhabmundi <surkhabm1@gmail.com>
2024-10-03 02:24:14 -0600

    ignore long file name. coded random number generator using import random

---
+++ randomnumbergenerator.py
@@ -0,0 +1,9 @@
+import random
+
+point1 = int(input("Please enter first interval end point: "))
+point2 = int(input("Please enter second interval end point: "))
+
+print(random.randint(point1, point2))
+
+
+
