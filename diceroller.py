b502e8c9f200c87dcd007d414aadd6678330d7c2
surkhabmundi <surkhabm1@gmail.com>
2024-10-04 01:19:19 -0600

    Following tutorial on dice roller program!

---
+++ diceroller.py
@@ -0,0 +1,50 @@
+import random
+
+dice_art = {
+    1: ("┌─────────┐",
+        "│         │",
+        "│    ●    │",
+        "│         │",
+        "└─────────┘"),
+    2: ("┌─────────┐",
+        "│  ●      │",
+        "│         │",
+        "│      ●  │",
+        "└─────────┘"),
+    3: ("┌─────────┐",
+        "│  ●      │",
+        "│    ●    │",
+        "│      ●  │",
+        "└─────────┘"),
+    4: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│         │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    5: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│    ●    │",
+        "│  ●   ●  │",
+        "└─────────┘"),
+    6: ("┌─────────┐",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "│  ●   ●  │",
+        "└─────────┘")
+}
+
+dice = []
+total = 0
+user_num = int(input("How many dice?: "))
+
+for die in range(user_num):
+    dice.append(random.randint(1, 6))
+
+for die in range(user_num):
+    for line in dice_art.get(dice[die]):
+        print(line)
+
+
+for die in dice:
+    total += die
+print(f"total: {total}")
