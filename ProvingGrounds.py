18401f39f239fb279536404506225917a1ceba91
surkhabmundi <surkhabm1@gmail.com>
2024-10-20 02:36:07 -0600

    Practicing functions and default arguments

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,4 +1,5 @@
-def net_price(list_price, discount, tax):
+def net_price(list_price, discount=0, tax=0.05):
     return list_price * (1 - discount) * (1 + tax)
 
-print(net_price(500, 0, 0.05))
+
+print(net_price(500))
