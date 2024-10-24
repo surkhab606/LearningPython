e285aa7ded93e19e8e58e9db938c43451209eb66
surkhabmundi <surkhabm1@gmail.com>
2024-10-24 17:43:05 -0600

    Dictionary has key:value pairs!

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -3,7 +3,7 @@ my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 for x in my_list:
     print(x, end=" ")
 
-my_dictionary = {1, 2, 3, 4, 5, 6}
+my_dictionary = {2, 3, 4, 5, 6}
 
 for x in my_dictionary:
     print(" ")
