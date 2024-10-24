d1b032dd0a382ab7d5db52f50e0fdb4ab44725e6
surkhabmundi <surkhabm1@gmail.com>
2024-10-24 17:42:32 -0600

    Practicing basic data structures.

---
+++ ProvingGrounds.py
@@ -0,0 +1,10 @@
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for x in my_list:
+    print(x, end=" ")
+
+my_dictionary = {1, 2, 3, 4, 5, 6}
+
+for x in my_dictionary:
+    print(" ")
+    print(x,end=" ")
