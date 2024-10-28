b4787fce9d2b6df5985ed0f5ca079d06dbe78c58
surkhabmundi <surkhabm1@gmail.com>
2024-10-28 15:33:05 -0600

    Weird approach, had some bugs. Going to refactor under one while loop!

---
+++ numberguessinggame.py
@@ -0,0 +1,26 @@
+import random
+
+user_range = int(input("Please enter the first number in the range of numbers you would like: "))
+user_range2 = int(input("Please enter the second number in the range of numbers you would like: "))
+
+comp_number = random.randint(user_range, user_range2)
+
+print("The Computer has selected a random number within your range.")
+user_guess = int(input("Please guess the number: "))
+
+while user_guess < user_range or user_guess > user_range2:
+    print("Out of bounds. Try again!")
+    user_guess = int(input("Please guess the number again: "))
+
+while user_guess > comp_number:
+    print("Too high! Try again!")
+    user_guess = int(input("Please guess the number again: "))
+
+while user_guess < comp_number:
+    print("Too low! Try again!")
+    user_guess = int(input("Please guess the number again: "))
+
+if user_guess == comp_number:
+    print("You got it! Good job!")
+
+
