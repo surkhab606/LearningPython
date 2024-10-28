918ff6273dcad8ff5cc19f21c3317570f7769058
surkhabmundi <surkhabm1@gmail.com>
2024-10-28 15:21:21 -0600

    Coding an alternate version of the number guessing game.

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,10 +1,7 @@
 my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+my_list.insert(1,3)
+my_list[0] = 69
 for x in my_list:
     print(x, end=" ")
 
-my_dictionary = {2, 3, 4, 5, 6}
-
-for x in my_dictionary:
-    print(" ")
-    print(x,end=" ")
