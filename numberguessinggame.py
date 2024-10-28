b3c157fb256115dec9676e02be21081431dd2992
surkhabmundi <surkhabm1@gmail.com>
2024-10-28 15:37:37 -0600

    Yes! It works. One problem: the out of bounds loop is a bit silly. gonna try to fix that, as well as have the print statement display the user's range in case they forgot.

--- numberguessinggame.py
+++ numberguessinggame.py
@@ -10,17 +10,16 @@ user_guess = int(input("Please guess the number: "))
 
 while user_guess != comp_number:
 
+    if user_guess < user_range or user_guess > user_range2:
+        print("Out of bounds. Try again!")
+        user_guess = int(input("Please guess the number: "))
+
     if user_guess > comp_number:
-         print("Too high! Try again!")
-         user_guess = int(input("Please guess the number: "))
+        print("Too high! Try again!")
+        user_guess = int(input("Please guess the number: "))
 
     if user_guess < comp_number:
         print("Too low! Try again!")
         user_guess = int(input("Please guess the number: "))
 
-    if user_guess < user_range or user_guess > user_range2:
-        print("Out of bounds. Try again!")
-        user_guess = int(input("Please guess the number: "))
-
 print("You got it!")
-
