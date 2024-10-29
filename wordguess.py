3eaf00b419e8c0cf06ec420220fe516ab6d3bc74
surkhabmundi <surkhabm1@gmail.com>
2024-10-29 17:36:31 -0600

    Established a basic loop that can be played, but would lose engagement quickly. Going to give more help to the user after a wrong guess to motivate them to the correct answer, but also simultaneously give them a limited amount of words to make it fair.

--- wordguess.py
+++ wordguess.py
@@ -4,4 +4,14 @@ word_list = ["rainbow", "strawberry", "games", "dinosaur", "incredible", "uranus
 
 comp_word = (random.choice(word_list))
 
-print(comp_word)
+user_guess = input("Please guess the word!: ")
+
+
+while user_guess != comp_word:
+    user_guess = input("Try again!: ")
+
+    if user_guess == comp_word:
+        print("You got it!")
+
+
+
