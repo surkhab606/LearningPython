807431ebabc3752b5e5e78a3b7ff419f21598ae6
surkhabmundi <surkhabm1@gmail.com>
2024-10-30 17:29:38 -0600

    Established user input to guess, hangman variable that holds how close the hangman is to dying, print statements for better understanding

--- hangman.py
+++ hangman.py
@@ -4,3 +4,11 @@ word_list = ["BMW", "UTAHRAPTOR", "CATERPILLAR", "DINOSAUR", "ELEPHANT", "MACBOO
              "LESBIAN", "KATE", "CANYON", "ABSOLUTE", "MAXIMUM", "MINIMUM"]
 
 comp_word = random.choice(word_list)
+spaces = len(comp_word)
+
+print(f"HANGMAN GAME!")
+print(f"The word is {spaces} long.")
+
+hangman = 0
+
+input("Please enter a letter to guess: ")
