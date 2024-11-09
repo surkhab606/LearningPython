0ed2c777ea736d01226cfc7a49213508478423f2
surkhabmundi <surkhabm1@gmail.com>
2024-11-08 17:28:40 -0700

    Practicing python

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -4,6 +4,10 @@ grocery_list = ["banana", "orange"]
 for i, item in enumerate(grocery_list):
     print(i, item)
 
+
 i = 0
-i += 1
-i -= 1
+
+while i < len(grocery_list):
+    print(grocery_list)
+    i += 1
+
