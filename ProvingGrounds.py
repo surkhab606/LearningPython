a9d29616aa65f709cc94a6aa7304cb9df2dfe709
surkhabmundi <surkhabm1@gmail.com>
2024-11-08 17:24:35 -0700

    Practicing python

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,7 +1,5 @@
 
 grocery_list = ["banana", "orange"]
-grocery_list.append("G80  M3")
 
-
-for i in range(len(grocery_list)):
-    print(grocery_list[i])
+for i, item in enumerate(grocery_list):
+    print(i, item)
