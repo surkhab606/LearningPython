2d10e468a2bc5b49a6e82c5c64d3aa73434d283e
surkhabmundi <surkhabm1@gmail.com>
2024-11-13 15:32:48 -0700

    Practicing python

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -2,6 +2,27 @@
 new_list = ["Johnson", "Canyon", "Mountain", "Leaf-Springs"]
 
 
-for x, item in enumerate(new_list):
-    print(x, item)
+age = int(input("Please enter their age: "))
 
+
+while age < 0:
+    print("Invalid input. Please try again.")
+    age = int(input("Please enter their age: "))
+
+if 0 <= age <= 5:
+    print("Baby!")
+
+elif 6 <= age <= 10:
+    print("Kid!")
+
+elif 11 <= age <= 13:
+    print("Tween!")
+
+elif 14 <= age <= 17:
+    print("Teen!")
+
+elif 18 <= age <= 25:
+    print("Young Adult!")
+
+elif 26 <= age <= 40:
+    print("Adult!")
