c81475be6c9c4b1082a94a76ab71b2d33a0b222a
surkhabmundi <surkhabm1@gmail.com>
2024-11-13 15:36:22 -0700

    Made my own print function lol

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,28 +1,8 @@
-
 new_list = ["Johnson", "Canyon", "Mountain", "Leaf-Springs"]
 
 
-age = int(input("Please enter their age: "))
+def our_print(s):
+    print(s)
 
 
-while age < 0:
-    print("Invalid input. Please try again.")
-    age = int(input("Please enter their age: "))
-
-if 0 <= age <= 5:
-    print("Baby!")
-
-elif 6 <= age <= 10:
-    print("Kid!")
-
-elif 11 <= age <= 13:
-    print("Tween!")
-
-elif 14 <= age <= 17:
-    print("Teen!")
-
-elif 18 <= age <= 25:
-    print("Young Adult!")
-
-elif 26 <= age <= 40:
-    print("Adult!")
+our_print("Johnson")
