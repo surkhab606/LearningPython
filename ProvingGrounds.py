08b2401ebc8cbed7bb2c1b1eb1a639431d426171
surkhabmundi <surkhabm1@gmail.com>
2024-11-14 20:16:29 -0700

    Cool function tricks

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -1,25 +1,5 @@
-new_list = ["Johnson", "Canyon", "Mountain", "Leaf-Springs"]
 
+def add(a, b):
+    return a + b
 
-def append_4_to_list(lst):
-    lst.append(4)
-
-
-def our_print(s):
-    print(s)
-
-
-append_4_to_list(new_list)
-print(new_list)
-
-b = new_list
-
-print(our_print("Johnson"))
-
-print(b is new_list)
-
-from copy import deepcopy
-
-b = deepcopy(new_list)
-
-print(b)
+print(add(1,100))
