f7ab2096a358de11092376ae876324ec3f6eb886
surkhabmundi <surkhabm1@gmail.com>
2024-11-14 20:04:41 -0700

    Messing around with lists and functions

--- ProvingGrounds.py
+++ ProvingGrounds.py
@@ -5,9 +5,15 @@ def append_4_to_list(lst):
     lst.append(4)
 
 
+def our_print(s):
+    print(s)
+
+
 append_4_to_list(new_list)
 print(new_list)
 
 b = new_list
+
+print(our_print("Johnson"))
 
 print(b is new_list)
